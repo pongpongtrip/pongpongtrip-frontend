e795d5d1bfaccee23a73d1f3a12a4bbc8fc85acd
lecocococo <chemwang13@naver.com>
2023-05-21 03:32:40 +0900

    Feat: 핫플레이스 기능 추가
    첫 화면에선 조회수 기준 핫플을 보여줌.
    검색시엔 검색된 결과를 조회수에 따라 화면에 보여줌
    스크롤시 3개씩 증가되면서 보여짐
    
    Fixes: #18

--- src/views/HotPlaceView.vue
+++ src/views/HotPlaceView.vue
@@ -2,12 +2,42 @@
   <div class="hotplace">
     <h2 class="mt-4">핫 플레이스</h2>
     <b-container>
+      <b-form inline class="justify-content-md-center" @submit="onSubmit">
+				<b-form-group id="search-area" class="mb-2 mr-sm-2 mb-sm-0">
+					<b-form-select
+						id="search-area"
+						v-model="form.sido_code"
+						:options="sido_codes"
+						required
+					></b-form-select>
+				</b-form-group>
+
+				<b-form-group id="search-content-id" class="mb-2 mr-sm-2 mb-sm-0">
+					<b-form-select
+						id="search-content-id"
+						v-model="form.content_type_id"
+						:options="content_type_ids"
+						required
+					></b-form-select>
+				</b-form-group>
+
+        <b-form-group id="search-keyword" class="mb-2 mr-sm-2 mb-sm-0">
+					<b-form-input
+						id="search-keyword"
+						v-model="form.search_keyword"
+						type="search"
+						placeholder="검색어"
+						required
+					></b-form-input>
+				</b-form-group>
+				<b-button type="submit" variant="primary">검색</b-button>
+			</b-form>
       <div class="card-row">
         <b-card
-          v-for="(card, index) in cards"
+          v-for="(card, index) in visibleCards"
           :key="index"
           :title="card.title"
-          :img-src="card.imgSrc"
+          :img-src="card.first_image"
           :img-alt="card.imgAlt"
           img-top
           tag="article"
@@ -15,7 +45,7 @@
           class="mb-2 card"
         >
         
-          <b-card-text>{{ card.content }}</b-card-text>
+          <b-card-text>{{ card.addr1 }}</b-card-text>
           <div
             class="icon-container"
             :class="{ active: card.currentIcon === 'heart-fill' }"
@@ -32,7 +62,10 @@
               class="heart-fill-icon"
             ></b-icon>
           </div>
-          <b-button href="#" variant="primary" class="go-button">Go somewhere</b-button>
+          <b-button @click="openModal" variant="primary" class="go-button">자세히 보기</b-button>
+          <div :class="{'custom-overlay': modalVisible}">
+            <ModalComponent :modalVisible="modalVisible" @close-modal="closeModal" />
+          </div>
         </b-card>
       </div>
     </b-container>
@@ -40,84 +73,97 @@
 </template>
 
 <script>
+import http from "@/api/httpDefault.js";
+import Vue from 'vue';
+import ModalComponent from '@/components/AttractionModal.vue'
+
 export default {
+  components: {
+    ModalComponent,
+  },
   data() {
     return {
+      visibleCount: 9,
+      visibleCards: [],
+      modalVisible: false,
+      form: {
+				sido_code: 0,
+				content_type_id: 0,
+        search_keyword: '',
+			},
+			sido_codes: [
+				{ text: '검색 할 지역 선택', value: 0 },
+				{ text: '서울', value: 1 },
+				{ text: '인천', value: 2 },
+				{ text: '대전', value: 3 },
+				{ text: '대구', value: 4 },
+				{ text: '광주', value: 5 },
+				{ text: '부산', value: 6 },
+				{ text: '울산', value: 7 },
+				{ text: '세종특별자치시', value: 8 },
+				{ text: '경기도', value: 31 },
+				{ text: '강원도', value: 32 },
+				{ text: '충청북도', value: 33 },
+				{ text: '충청남도', value: 34 },
+				{ text: '경상북도', value: 35 },
+				{ text: '경상남도', value: 36 },
+				{ text: '전라북도', value: 37 },
+				{ text: '전라남도', value: 38 },
+				{ text: '제주도', value: 39 },
+			],
+			content_type_ids: [
+				{ text: '관광지 유형', value: 0 },
+				{ text: '관광지', value: 12 },
+				{ text: '문화시설', value: 14 },
+				{ text: '축제공연행사', value: 15 },
+				{ text: '여행코스', value: 25 },
+				{ text: '레포츠', value: 28 },
+				{ text: '숙박', value: 32 },
+				{ text: '쇼핑', value: 38 },
+				{ text: '음식점', value: 39 },
+			],
       cards: [
-        {
-          title: "Card Title 1",
-          imgSrc: "https://picsum.photos/600/300/?image=25",
-          imgAlt: "Image",
-          content:
-            "123",
-          currentIcon: "heart",
-        },
-        {
-          title: "Card Title 2",
-          imgSrc: "https://picsum.photos/600/300/?image=25",
-          imgAlt: "Image",
-          content:
-            "Some quick example text to build on the card title and make up the bulk of the card's content.",
-          currentIcon: "heart",
-        },
-        {
-          title: "Card Title 3",
-          imgSrc: "https://picsum.photos/600/300/?image=25",
-          imgAlt: "Image",
-          content:
-            "Some quick example text to build on the card title and make up the bulk of the card's content.",
-          currentIcon: "heart",
-        },
-        {
-          title: "Card Title 4",
-          imgSrc: "https://picsum.photos/600/300/?image=25",
-          imgAlt: "Image",
-          content:
-            "Some quick example text to build on the card title and make up the bulk of the card's content.",
-          currentIcon: "heart",
-        },
-        {
-          title: "Card Title 5",
-          imgSrc: "https://picsum.photos/600/300/?image=25",
-          imgAlt: "Image",
-          content:
-            "Some quick example text to build on the card title and make up the bulk of the card's content.",
-          currentIcon: "heart",
-        },
-        {
-          title: "Card Title 6",
-          imgSrc: "https://picsum.photos/600/300/?image=25",
-          imgAlt: "Image",
-          content:
-            "Some quick example text to build on the card title and make up the bulk of the card's content.",
-          currentIcon: "heart",
-        },
-        {
-          title: "Card Title 7",
-          imgSrc: "https://picsum.photos/600/300/?image=25",
-          imgAlt: "Image",
-          content:
-            "Some quick example text to build on the card title and make up the bulk of the card's content.",
-          currentIcon: "heart",
-        },
-        {
-          title: "Card Title 8",
-          imgSrc: "https://picsum.photos/600/300/?image=25",
-          imgAlt: "Image",
-          content:
-            "Some quick example text to build on the card title and make up the bulk of the card's content.",
-          currentIcon: "heart",
-        },
-        // Add more card objects as needed
+        // {
+        //   title: "Card Title 1",
+        //   imgSrc: "https://picsum.photos/600/300/?image=25",
+        //   imgAlt: "Image",
+        //   content:
+        //     "123",
+        //   currentIcon: "heart",
+        // },
+        
       ],
     };
   },
+  created() {
+    http
+      .post("/attraction/hotplace")
+      .then((response) => {
+        this.cards = response.data;
+        
+        this.cards.forEach(item => {
+          if (item.first_image === ""){
+            console.log("이미지 없음");
+            item.first_image = "https://picsum.photos/600/300/?image=25";
+          }
+          item.imgAlt = "https://picsum.photos/600/300/?image=25";
+          item.currentIcon = "heart";
+        });
+
+
+        this.updateVisibleCards();
+      });
+
+},
+
   mounted() {
-    this.arrangeCards();
-    window.addEventListener("resize", this.arrangeCards);
+    // this.arrangeCards();
+    // window.addEventListener("resize", this.arrangeCards);
+    window.addEventListener("scroll", this.loadMoreCards);
   },
   beforeDestroy() {
-    window.removeEventListener("resize", this.arrangeCards);
+    // window.removeEventListener("resize", this.arrangeCards);
+    window.removeEventListener("scroll", this.loadMoreCards);
   },
   methods: {
     arrangeCards() {
@@ -128,16 +174,80 @@ export default {
           const rowIndex = Math.floor(index / 3);
           card.style.order = rowIndex;
         });
+      
+      }
+    },
+    updateVisibleCards() {
+      this.visibleCards = this.cards.slice(0, this.visibleCount);
+      console.log(this.visibleCards);
+    },
+    loadMoreCards() {
+      console.log("scroll");
+      console.log(this.visibleCards.length  )
+      const windowScrollY = window.scrollY || window.pageYOffset;
+      const windowHeight = window.innerHeight;
+      const documentHeight = document.documentElement.scrollHeight;
+
+      // 스크롤이 페이지 맨 아래 또는 가까이에 도달했을 때
+      if (windowScrollY + windowHeight + 130 >= documentHeight) {
+        // 더 많은 카드를 보여줄 수 있는지 확인
+        if (this.visibleCount + 3 <= this.cards.length) {
+          this.visibleCount += 3;
+          this.updateVisibleCards();
+        } else {
+          // 모든 카드를 보여줌
+          this.visibleCount = this.cards.length;
+          this.updateVisibleCards();
+        }
       }
     },
     toggleIcon(card) {
-      card.currentIcon = card.currentIcon === 'heart' ? 'heart-fill' : 'heart';
-      card.iconColor = card.currentIcon === 'heart-fill' ? 'pink' : 'black';
+      // card.currentIcon = card.currentIcon === 'heart' ? 'heart-fill' : 'heart';
+      // card.iconColor = card.currentIcon === 'heart-fill' ? 'pink' : 'black';
+      // this.$forceUpdate();
+      if (card.currentIcon === 'heart') {
+        Vue.set(card, 'currentIcon', 'heart-fill');
+        Vue.set(card, 'iconColor', 'pink');
+      } else {
+        Vue.set(card, 'currentIcon', 'heart');
+        Vue.set(card, 'iconColor', 'black');
+      }
     },
-    // toggleIcon(card) {
-    //   card.currentIcon = card.currentIcon === "heart" ? "heart-fill" : "heart";
-    //   card.iconColor = card.currentIcon === "heart-fill" ? "red" : "";
-    // },
+
+    onSubmit(event) {
+			event.preventDefault();
+			alert(JSON.stringify(this.form));
+			http
+				.post(`/attraction/hotplace/search`, JSON.stringify(this.form))
+				.then((response) => {
+					console.log(response.data);
+
+					// 받아온 데이터를 가공하여 tripItems에 할당
+					this.hotItems = response.data;
+					
+          this.cards = response.data;
+        
+          this.cards.forEach(item => {
+            if (item.first_image === ""){
+              console.log("이미지 없음");
+              item.first_image = "https://picsum.photos/600/300/?image=25";
+            }
+            item.imgAlt = "https://picsum.photos/600/300/?image=25";
+            item.currentIcon = "heart";
+          });
+
+
+          this.updateVisibleCards();
+				});
+		},
+    openModal() {
+      this.modalVisible = true;
+      document.body.classList.add("modal-open"); // 흐림 효과를 위한 클래스 추가
+    },
+    closeModal() {
+      this.modalVisible = false;
+      document.body.classList.remove("modal-open"); // 흐림 효과를 위한 클래스 제거
+    },
 
   },
 };
@@ -153,6 +263,13 @@ export default {
   flex-basis: calc(33.33% - 1rem);
   margin: 0.5rem;
 }
+
+.card img {
+  width: 100%;
+  height: 200px; /* Adjust the desired height */
+  object-fit: cover; /* Maintain aspect ratio and cover the container */
+}
+
 .card .card-text {
   font-size: 14px;
 }
@@ -211,5 +328,11 @@ export default {
 
 .go-button {
   z-index: 1;
+}
+
+.modal-open {
+  /* 흐림 효과 스타일 설정 */
+  backdrop-filter: blur(10px);
+  background-color: rgba(0, 0, 0, 0.6);
 }
 </style>
