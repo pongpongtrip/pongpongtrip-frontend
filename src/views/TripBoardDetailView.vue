789c40c9c9a8da46c8a9bac4ead2bc43dd9b5451
이찬민 <chemwang13@naver.com>
2023-05-23 16:20:12 +0900

    Feat: 공유하기 기능 및 글 쓰기, 조회 시 여행계획을 볼 수 있는 기능
    - 마이페이지에서 여행계획 공유하기 클릭시 글쓰기 화면으로 넘어가며 여행계획에 대한 글쓰기가 가능
    - 여행계획 글쓰기를 할때 여행 계획을 보여주는 기능
    - 여행계획에 대한 글을 상세보기 할때 여행 계획을 보여주는 기능
    
    Resolves: #34

--- src/views/TripBoardDetailView.vue
+++ src/views/TripBoardDetailView.vue
@@ -27,8 +27,41 @@
                 <p class="right text-secondary fw-light">{{ article.registerTime }} 조회: {{ article.hit }}</p>
   <!--               <div class="col-md-4 align-self-center text-end">댓글 : 17</div> -->
                 <hr class="mb-3" />
+
+                <!-- 카드 스와이퍼 -->
+                <h4 style="text-align:left;">공유할 계획</h4>
+                <swiper class="swiper travel-plan-swiper" :options="swiperOption">
+                  <swiper-slide v-for="place in plan" :key="place.content_id">
+                    <b-card
+                      :title="place.title"
+                      :img-src="place.first_image"
+                      :img-alt="place.imgAlt"
+                      img-top
+                      tag="article"
+                      style="width: 18rem"
+                      class="mb-2 card"
+                    >
+                      <b-card-text class="writeText time">{{ place.time }}</b-card-text>
+                      <b-card-text class="writeText">{{ place.addr1 }}</b-card-text>
+                      <b-button
+                        @click="openModal(place)"
+                        variant="primary"
+                        class="go-button"
+                        id="btn_primary"
+                        >자세히 보기</b-button
+                      >
+                    </b-card>
+                  </swiper-slide>
+
+                  <!-- 이전(prev) 및 다음(next) 버튼 -->
+                  <div class="swiper-button-prev" slot="button-prev"></div>
+                  <div class="swiper-button-next" slot="button-next"></div>
+                </swiper>
+
+
                 <div class="content text-secondary">
-                  {{ article.content }}
+                  <!-- 줄 바꿈 적용 코드 -->
+                  <div v-for="el in article.content.split('\n')" :key="el"> {{ el }} <br /></div>
                 </div>
                 <div v-if="article.fileInfos" class="mt-3">
                   <ul>
@@ -50,16 +83,32 @@
           </div>
         </div>
       </main>
+
+      <b-modal v-model="modalOpen" @hidden="closeModal" size="lg" dialog-class="custom-modal">
+        <template #modal-title>{{ selectedCard.title }}</template>
+        <template #default>
+          <img :src="selectedCard.first_image" :alt="selectedCard.imgAlt" style="max-width: 100%" />
+          <p>주소: {{ selectedCard.addr1 }}</p>
+          <p>전화번호: {{ selectedCard.telname }}</p>
+          <p>{{ selectedCard.overview }}</p>
+          <KaKaoMap ref="kakaoMap" :marker-items="markers" />
+        </template>
+      </b-modal>
     </div>
   </template>
   
   <script>
+  import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+  import "swiper/css/swiper.css";
   import http from "@/api/httpDefault.js";
+  import KaKaoMap from "@/components/KaKaoMap.vue";
   
   export default {
     name: "boardDeatil",
     components: {
-
+      Swiper,
+      SwiperSlide,
+      KaKaoMap,
     },
     props: {
           detail: {
@@ -68,6 +117,21 @@
     },
     data() {
       return {
+        plan: [],
+        modalOpen: false,
+        selectedCard: null,
+        swiperOption: {
+          slidesPerView: 3,
+          spaceBetween: 15,
+          autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+          },
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+        },
         article: {  // Replace with your article object
           articleNo: 1,
           subject: 'Sample Subject',
@@ -92,16 +156,19 @@
 					articleNo:this.$store.state.articleNo,
 				}})
               .then((response) => {
-                    console.log(response.data);
-                    this.article.articleNo = response.data.articleNo;
-                    this.article.subject = response.data.subject;
-                    this.article.userId = response.data.userId;
-                    this.article.registerTime = response.data.registerTime;
-                    this.article.hit = response.data.hit;
-                    this.article.content = response.data.content;
-                    this.article.fileInfos = response.data.fileInfos;
+                  console.log(response.data);
+                  this.article.articleNo = response.data.articleNo;
+                  this.article.subject = response.data.subject;
+                  this.article.userId = response.data.userId;
+                  this.article.registerTime = response.data.registerTime;
+                  this.article.hit = response.data.hit;
+                  this.article.content = response.data.content;
+                  this.article.fileInfos = response.data.fileInfos;
 
-                    this.userinfo.userId = response.data.userId;
+                  this.userinfo.userId = response.data.userId;
+                  this.plan = JSON.parse(response.data.plan);
+
+                  console.log(this.plan);
               });
     },
     computed: {
@@ -136,6 +203,27 @@
                 });
         }
       },
+      openModal(card) {
+        console.log(card);
+        this.selectedCard = card;
+
+        this.modalOpen = true;
+        this.markers = [[card.latitude, card.longitude]];
+        console.log(this.markers);
+
+        // console.log("선택한 카드 인덱스:", index);
+      },
+
+      closeModal() {
+        this.modalOpen = false;
+        this.markers = [];
+        this.selectedCard = null;
+        const kakaoMapComponent = this.$refs.kakaoMap;
+        if (kakaoMapComponent) {
+          // kakaoMapComponent을 사용하여 초기화 작업 수행
+          kakaoMapComponent.init();
+        }
+      },
     },
   };
   </script>
@@ -151,4 +239,55 @@
   .right {
     text-align: right;
   }
+
+  .swiper {
+  width: 100%;
+  max-height: 600px; /* Set the maximum height of the swiper container */
+}
+
+.swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  background: #fff;
+  padding: 10px;
+  max-width: 20rem; /* Set the maximum width of each card */
+  margin: 0 auto; /* Center the cards horizontally */
+  box-sizing: border-box; /* Include padding and border in the width */
+}
+
+.card-body {
+  display: flex;
+  flex-direction: column;
+  height: 220px;
+}
+
+.card-title {
+  font-size: 14pt;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+.card-text {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.card-body .time {
+  margin-top: auto !important;
+}
+
+.writeText {
+  font-size: 10px; /* Set the desired font size for the card content */
+}
+
+.writeText {
+  font-size: 10px; /* Set the desired font size for the card content */
+}
+#form-register {
+    width: 60%;
+}
+button {
+    margin: 2px;
+}
   </style>
